37efdb1417c3bb5f5ee53584ece21d0d56057e6d
Marko Kosunen <marko.kosunen@aalto.fi>
2021-01-30 05:51:30 +0200

    Init git-versioned submodule register_template

---
+++ src/main/scala/register_template/register_template.scala
@@ -0,0 +1,54 @@
+
+// Dsp-block register_template
+// Description here
+// Inititally written by dsp-blocks initmodule.sh, 20210130
+package register_template
+
+import chisel3.experimental._
+import chisel3._
+import dsptools.{DspTester, DspTesterOptionsManager, DspTesterOptions}
+import dsptools.numbers._
+import breeze.math.Complex
+
+class register_template_io[T <:Data](proto: T,n: Int)
+   extends Bundle {
+        val A       = Input(Vec(n,proto))
+        val B       = Output(Vec(n,proto))
+        override def cloneType = (new register_template_io(proto.cloneType,n)).asInstanceOf[this.type]
+   }
+
+class register_template[T <:Data] (proto: T,n: Int) extends Module {
+    val io = IO(new register_template_io( proto=proto, n=n))
+    val register=RegInit(VecInit(Seq.fill(n)(0.U.asTypeOf(proto.cloneType))))
+    register:=io.A
+    io.B:=register
+}
+
+//This gives you verilog
+object register_template extends App {
+    chisel3.Driver.execute(args, () => new register_template(
+        proto=DspComplex(UInt(16.W),UInt(16.W)), n=8)
+    )
+}
+
+//This is a simple unit tester for demonstration purposes
+class unit_tester(c: register_template[DspComplex[UInt]] ) extends DspTester(c) {
+//Tests are here 
+    poke(c.io.A(0).real, 5)
+    poke(c.io.A(0).imag, 102)
+    step(5)
+    fixTolLSBs.withValue(1) {
+        expect(c.io.B(0).real, 5)
+        expect(c.io.B(0).imag, 102)
+    }
+}
+
+//This is the test driver 
+object unit_test extends App {
+    iotesters.Driver.execute(args, () => new register_template(
+            proto=DspComplex(UInt(16.W),UInt(16.W)), n=8
+        )
+    ){
+            c=>new unit_tester(c)
+    }
+}
